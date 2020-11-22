43e6f6410b312ef250f60b7470d5d859e6f83e04
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-11-22 23:55:16 +0900

    feat: detail page

---
+++ bookscanner_image_processing/src/page/Detail/index.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+const getLocation = () => {}
+
+const drawSection = () => {}
+
+export const Detail = () => {
+  return (
+    <div className="detail page">
+      <img src="../../../assets/Polygon.png" />
+      <p>Go Back</p>
+      <p>Adjust Image</p>
+      <div className="showImg"></div>
+      <div className="adjustImg"></div>
+    </div>
+  )
+}
