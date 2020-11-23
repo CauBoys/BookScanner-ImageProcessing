0e68392bc45799118234ebc189a9244d56d31b41
bini0823 <bini0823@cau.ac.kr>
2020-11-23 14:17:44 +0900

    초기 route setting

--- bookscanner_image_processing/src/App.js
+++ bookscanner_image_processing/src/App.js
@@ -1,7 +1,28 @@
+import React from 'react'
+import {
+  Link,
+  Route,
+  useHistory,
+  Switch,
+  BrowserRouter as Router,
+} from 'react-router-dom'
+import Home from './pages/home'
+import intro from './pages/intro'
+import detail from './pages/detail'
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <Router>
+      <main>
+        <Switch>
+          <Route exact path="/" component={intro} />
+          <Route path="/home" component={Home} />
+          <Route path="/detail" component={detail} />
+        </Switch>
+      </main>
+    </Router>
+  )
 }
 
 export default App
