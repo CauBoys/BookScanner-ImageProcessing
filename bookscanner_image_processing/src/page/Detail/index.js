5d307968a12e144ce4fcbcf6448690e470c5ffb8
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-11-23 23:54:24 +0900

    feat: Detail page 추가

--- bookscanner_image_processing/src/page/Detail/index.js
+++ bookscanner_image_processing/src/page/Detail/index.js
@@ -7,11 +7,23 @@ const drawSection = () => {}
 export const Detail = () => {
   return (
     <div className="detail page">
-      <img src="../../../assets/Polygon.png" />
-      <p>Go Back</p>
-      <p>Adjust Image</p>
-      <div className="showImg"></div>
-      <div className="adjustImg"></div>
+      <div className="sub_title">
+        <img src="../Polygon.png" className="btn_back" />
+        <p class="go_back">Go Back</p>
+      </div>
+      <div class="title">Adjust Image</div>
+      <div className="content">
+        <div className="showImg"></div>
+        <div className="adjustImg">
+          <div className="adjust">
+            <p className="adjust_title">const</p>
+            <div className="adjust_content">---------------</div>
+            <p>Erase</p>
+            <img src="../border_color.png" className="adjust_content erase" />
+          </div>
+          <button className="btn">Save Change</button>
+        </div>
+      </div>
     </div>
   )
 }
