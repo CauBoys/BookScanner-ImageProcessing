297bccdc1e4b02b85c0ddb262cef42e6eb61416c
bini0823 <bini0823@cau.ac.kr>
2020-11-23 19:00:43 +0900

    버튼 bootstrap의 버튼으로 변경

--- bookscanner_image_processing/src/components/nextButton.js
+++ bookscanner_image_processing/src/components/nextButton.js
@@ -1,10 +1,13 @@
 import { useHistory } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 
 export const NextButtons = ({ path, name }) => {
   const history = useHistory()
   return (
     <div>
-      <button onClick={() => history.push(path)}>{name}</button>
+      <Button variant="primary" onClick={() => history.push(path)}>
+        {name}
+      </Button>
     </div>
   )
 }
