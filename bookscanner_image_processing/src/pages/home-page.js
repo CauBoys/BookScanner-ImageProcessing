26783e7c996a6aceadfdc54a89a50bd29ac49eeb
bini0823 <bini0823@cau.ac.kr>
2020-11-23 20:43:47 +0900

    file 업로드 기능 구현(이미지 파일만 업로드가능)

--- bookscanner_image_processing/src/pages/home-page.js
+++ bookscanner_image_processing/src/pages/home-page.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { NextButtons } from '../components/nextButton'
 import { Button, Card, ProgressBar } from 'react-bootstrap'
 import { FaPlusCircle } from 'react-icons/fa'
@@ -8,7 +8,17 @@ import testImg from '../assets/logo.png'
 import '../style/home.css'
 
 export default function Home() {
+  const [image, setImage] = useState()
   const [contrastValue, setContrastValue] = useState(0)
+  const hiddenFileInput = useRef(null)
+
+  const handleClick = (event) => {
+    hiddenFileInput.current.click()
+  }
+  const handleChange = (event) => {
+    const fileUploaded = event.target.files[0]
+    setImage(fileUploaded)
+  }
   return (
     <div className="Container">
       <div className="Header">
@@ -39,9 +49,20 @@ export default function Home() {
         </Card>
         <Card className="Card Card-Add">
           <Card.Body className="Card-Body">
-            <Button variant="Dark" className="AddButton-Card">
+            <Button
+              variant="Dark"
+              className="AddButton-Card"
+              onClick={handleClick}
+            >
               <FaPlusCircle size={40} />
             </Button>
+            <input
+              type="file"
+              ref={hiddenFileInput}
+              onChange={handleChange}
+              accept="image/*"
+              style={{ display: 'none' }}
+            />
           </Card.Body>
         </Card>
       </div>
