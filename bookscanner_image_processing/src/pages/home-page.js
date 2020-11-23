aa115e5ffa662b8584f36b03bfe56bd866eac3f3
bini0823 <bini0823@cau.ac.kr>
2020-11-23 20:12:29 +0900

    이미지 추가 card 구현

--- bookscanner_image_processing/src/pages/home-page.js
+++ bookscanner_image_processing/src/pages/home-page.js
@@ -4,10 +4,11 @@ import { Button, Card, ProgressBar } from 'react-bootstrap'
 import { FaPlusCircle } from 'react-icons/fa'
 import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css'
 import RangeSlider from 'react-bootstrap-range-slider'
+import testImg from '../assets/logo.png'
 import '../style/home.css'
+
 export default function Home() {
   const [contrastValue, setContrastValue] = useState(0)
-
   return (
     <div className="Container">
       <div className="Header">
@@ -18,11 +19,7 @@ export default function Home() {
       </div>
       <div className="Body-Container">
         <Card className="Card">
-          <Card.Img
-            className="Card-Image"
-            variant="top"
-            src="../../assets/logo.png/100px180"
-          />
+          <Card.Img className="Card-Image" variant="top" src={testImg} />
           <Card.Body className="Card-Body">
             <div className="Slider-Container">
               <div className="Slider-Title">contrast</div>
@@ -35,7 +32,16 @@ export default function Home() {
                 />
               </div>
             </div>
-            <NextButtons path="/detail" name="Adjust Image" />
+            <div className="Next-Button">
+              <NextButtons path="/detail" name="Adjust Image" />
+            </div>
+          </Card.Body>
+        </Card>
+        <Card className="Card Card-Add">
+          <Card.Body className="Card-Body">
+            <Button variant="Dark" className="AddButton-Card">
+              <FaPlusCircle size={40} />
+            </Button>
           </Card.Body>
         </Card>
       </div>
