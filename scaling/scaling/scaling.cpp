d0f264d8f97cae59f20e41c32ea89799b59137d4
youngwoon <youngwoon96@naver.com>
2020-11-26 05:22:10 +0900

    feat: show pictures

--- scaling/scaling/scaling.cpp
+++ scaling/scaling/scaling.cpp
@@ -20,6 +20,56 @@ void show(Mat image) {
     waitKey(0);
 }
 
+
+double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0) {
+    double dx1 = pt1.x - pt0.x;
+    double dy1 = pt1.y - pt0.y;
+    double dx2 = pt2.x - pt0.x;
+    double dy2 = pt2.y - pt0.y;
+    return (dx1 * dx2 + dy1 * dy2) / sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2) + 1e-10);
+}
+
+void showPictures(Mat img) {
+    Mat src_gray, canny_output;
+    RNG rng(12345);
+    vector<Vec4i> hierarchy;
+    vector<vector<Point> > contours;
+    int thresh = 100;
+
+    /// Convert image to gray and blur it
+    cvtColor(img, src_gray, CV_BGR2GRAY);
+    blur(src_gray, src_gray, Size(3, 3));
+    erode(src_gray, src_gray, Mat());
+
+    // Find contours and store them in a list
+    Canny(src_gray, canny_output, thresh, thresh * 2, 3);
+    findContours(canny_output, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+
+    Mat mask = Mat::zeros(src_gray.rows, src_gray.cols, CV_8UC1);
+    for (size_t i = 0; i < contours.size(); i++)
+    {
+        Rect rect = boundingRect(contours[i]);
+        if (rect.width > img.rows / 20 & rect.height > img.cols / 20) {
+            rectangle(mask, rect, Scalar(100, 100, 100), -1);
+        }
+    }
+    findContours(mask, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+
+    /// Draw contours
+    Mat drawing = Mat::zeros(img.size(), CV_8UC3);
+    for (int i = 0; i < contours.size(); i++)
+    {
+        string loc = "output/" + file_name + "_" + to_string(i);
+        loc = loc + ".jpg";
+        Mat crop = img(boundingRect(cv::Mat(contours[i])));
+        imwrite(loc, crop);
+
+        Scalar color = Scalar(255, 0, 0);
+        drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
+    }
+
+}
+
 Mat rotate(vector<Point> not_a_rect_shape) {
 
     // Assemble a rotated rectangle out of that info
@@ -117,7 +167,7 @@ Mat arrange_image(Mat image)
         }
     }
     int i = 0;
-    if(squares.size() > 0) {
+    if (squares.size() > 0) {
         //it generally works without below for... if it doesn't work well for some image, than I'll check out.
         //for (int i = 0; i < squares.size(); i++) {
         // draw contour
@@ -140,7 +190,7 @@ Mat arrange_image(Mat image)
 
 int main(int argc, char** argv)
 {
-    CommandLineParser parser(argc, argv, "{@input | "+file_name+".jpg | input image}");
+    CommandLineParser parser(argc, argv, "{@input | " + file_name + ".jpg | input image}");
     src = imread(samples::findFile(parser.get<String>("@input")), IMREAD_COLOR); // Load an image
     if (src.empty())
     {
@@ -150,6 +200,7 @@ int main(int argc, char** argv)
     }
     Mat dst = src.clone();
     Mat square_img = arrange_image(dst);
+    showPictures(square_img);
 
     return 0;
 }
