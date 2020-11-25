6d50640ce3bb7286b98e5ba63af89545fa72d773
sshrik <chj8781942naver.com>
2020-11-25 23:57:47 +0900

    Add Additional function and shell files.

--- NodeRunner/nodeRunner.js
+++ NodeRunner/nodeRunner.js
@@ -37,4 +37,44 @@ function imageContrast(sourceFile, factor) {
     });
 }
 
-module.exports = { addWaterMark, imageContrast };
+function paperDetect(sourceFile) {
+    return new Promise((res, rej) => {
+        runShell('sh paperDetect.sh ' + sourceFile + " " + factor).
+        then((stdout) => {
+            res(stdout);
+        }).
+        catch((err) => {
+            rej(err);
+        })
+    });
+}
+// 사람이 올린 사진에서 종이부분을 찾는것.
+
+function imageDetect(sourceFile) {
+    return new Promise((res, rej) => {
+        runShell('sh imageDetect.sh ' + sourceFile).
+        then((stdout) => {
+            res(stdout);    // printf 로 잘라낸 사진을 저장하고, 그 파일 이름을 출력해야한다.
+            // 파일 이름은 bookscannner 어쩌구 폴더에 upload 폴더 하나 만들어서 그 안에 adjust?
+            // ~/upload/adjust/uuid_0.jpg -> 원본파일 : ~/upload/xxxx-xxxx-xxxx-xxxx-xxxx.jpg
+        }).
+        catch((err) => {
+            rej(err);
+        })
+    });
+}
+// 종이부분에서 사진 List를 찾는것.
+
+function imageBlur(sourceFile, startX, startY, endX, endY, backX, backY) {
+    return new Promise((res, rej) => {
+        runShell('sh imageBlur.sh ' + sourceFile + " " + String(startX) + " " + String(startY) + " " + String(endX) + " " + String(endY) + " " + String(backX) + " " + String(backY) + " ").
+        then((stdout) => {
+            res(stdout);
+        }).
+        catch((err) => {
+            rej(err);
+        })
+    });
+}
+
+module.exports = { addWaterMark, imageContrast, paperDetect, imageDetect, imageBlur };
