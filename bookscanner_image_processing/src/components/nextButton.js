2e7906c63d3740d8e61e32e682dbdbc57c4aae6b
bini0823 <bini0823@naver.com>
2020-11-28 23:38:29 +0900

    router로 이미지 id값 넘김

--- bookscanner_image_processing/src/components/nextButton.js
+++ bookscanner_image_processing/src/components/nextButton.js
@@ -1,11 +1,11 @@
 import { useHistory } from 'react-router-dom'
 import { Button } from 'react-bootstrap'
 
-export const NextButtons = ({ path, name }) => {
+export const NextButtons = ({ path, name, id }) => {
   const history = useHistory()
   return (
     <div>
-      <Button variant="primary" onClick={() => history.push(path)}>
+      <Button variant="primary" onClick={() => history.push(`${path}/${id}`)}>
         {name}
       </Button>
     </div>
