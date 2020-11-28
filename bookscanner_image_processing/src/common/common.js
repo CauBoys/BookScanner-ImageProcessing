7cb64429c18c12cbe1daeaf9ffeb076d04ee7b84
bini0823 <bini0823@naver.com>
2020-11-28 15:24:48 +0900

    서버 주소 변경

--- bookscanner_image_processing/src/common/common.js
+++ bookscanner_image_processing/src/common/common.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:5000/image'
+export const BASE_URL = 'http://192.168.35.163:46000/'
 
 export const DEEP_CLONE = (obj) => {
   if (obj === null || typeof obj !== 'object') {
