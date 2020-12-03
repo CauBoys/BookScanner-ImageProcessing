f8990d99e12ddedb28db841ca4308d9e7474149d
sshrik <chj8781942naver.com>
2020-12-03 12:00:23 +0900

    Error Correction at image processing total.

--- imageProcessing/ImageProcessingTotal/ImageProcessingTotal/imageProcessing.hpp
+++ imageProcessing/ImageProcessingTotal/ImageProcessingTotal/imageProcessing.hpp
@@ -1,10 +1,10 @@
 #pragma once
-#include "opencv2/opencv.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/types_c.h"
-#include "opencv2/imgproc/imgproc_c.h"
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/types_c.h>
+#include <opencv2/imgproc/imgproc_c.h>
 #include <vector>
 #include <iostream>
 #include <cstring>
